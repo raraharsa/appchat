8dc6babc0d00772da2f4dea1bf3d6a7516eb7fc7
Ariniputrimahira <159981802+raraharsa@users.noreply.github.com>
2024-10-30 09:35:11 +0700

    arin

--- re_message.php
+++ re_message.php
@@ -2,7 +2,7 @@
 require 'koneksi.php';
 
 $chat_id = $_GET['chat_id'];
-$stmt = $pdo->prepare("SELECT * FROM messages WHERE chat_id = ?");
+$stmt = $pdo->prepare("SELECT * FROM tbmessages WHERE chat_id = ?");
 $stmt->execute([$chat_id]);
 $messages = $stmt->fetchAll();
 
@@ -11,8 +11,24 @@ $messages = $stmt->fetchAll();
 <table>
     <tr>
         <th>ID</th>
-        <th></th>
-        <th></th>
-        <th></th>
+        <th>User</th>
+        <th>Message</th>
+        <th>Waktu</th>
     </tr>
+    <?php foreach ($messages as $message): ?>
+        <tr>
+            <td><?= $message['id'] ?>
+            </td>
+            <td>
+            <?php
+            $user_stmt = $pdo->prepare("SELECT username FROM tbusers WHERE id = ?");
+            $user_stmt->execute([$messages['user_id']]);
+            $user = $user_stmt->fetch();
+            echo $user['username'];
+            ?>
+            </td>
+           <td><?= $message['message'] ?></td> 
+           <td><?= $message['created_at'] ?></td>
+        </tr>
+        <?php endforeach; ?>
 </table>
