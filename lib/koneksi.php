b914ee711eb99db2238fd759522c415a05a76c10
Alya Elfarianti <alyaelfarianti@gmail.com>
2024-10-31 08:59:39 +0700

    alya

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -6,7 +6,7 @@ $pass = '123456789';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options =[
+$options = [
     PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES      => false,
