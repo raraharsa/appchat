93db7a2aedf27499a87a05aaa13007ead50031db
salsabilahnurkhalifah <salsabilahnurkhalifah4@gmail.com>
2024-10-31 10:14:22 +0700

    pemindahan

--- del_chat.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'lib/koneksi.php';
-
-$id = $_GET['id'];
-$stmt = $pdo->prepare("DELETE FROM tbchats WHERE id = ?");
-$stmt->execute([$id]);
-
-header("Location: re_chat.php");
-?>
---
+++ modul/del_chat.php
@@ -0,0 +1,9 @@
+<?php
+require 'lib/koneksi.php';
+
+$id = $_GET['id'];
+$stmt = $pdo->prepare("DELETE FROM tbchats WHERE id = ?");
+$stmt->execute([$id]);
+
+header("Location: re_chat.php");
+?>
