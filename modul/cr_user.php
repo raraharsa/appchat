ee86cf0f8e4e0364bbca89a5eba4405058bb2536
Ariniputrimahira <159981802+raraharsa@users.noreply.github.com>
2024-11-02 19:19:27 +0700

    aa

--- modul/cr_user.php
+++ modul/cr_user.php
@@ -1,5 +1,5 @@
 <?php
-include 'lib/config.php';
+include '../lib/config.php';
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -9,14 +9,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->execute([$username, $email]);
     header("Location: re_users.php"); exit();// Tambahkan `exit` untuk menghentikan eksekusi setelah redirect.
 } 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Koneksi gagal: " . $e->getMessage();
-}
-
-
 
 ?>
 <form method="POST">
