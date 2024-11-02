635a8f64f3deb7b31a1a55afa1c4c8f7090dd34a
Ariniputrimahira <159981802+raraharsa@users.noreply.github.com>
2024-11-02 19:04:40 +0700

    arin

--- lib/koneksi.php
+++ lib/koneksi.php
@@ -14,7 +14,7 @@ $options = [
 
 try{
     $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e){
+} catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
