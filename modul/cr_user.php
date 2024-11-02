f7f575f6b6221d88bf1ff33f37245de18065e9cc
raraharsa <aputrimahira@gmail.com>
2024-11-02 10:52:50 +0700

    arin

--- modul/cr_user.php
+++ modul/cr_user.php
@@ -7,9 +7,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $pdo->prepare("INSERT INTO tbusers (username, email) VALUES (?, ?)");
     $stmt->execute([$username, $email]);
     header("Location: re_users.php"); exit();// Tambahkan `exit` untuk menghentikan eksekusi setelah redirect.
-}
-if (file_exists('../lib/koneksi.php')) {
-    require '../lib/koneksi.php';
 } else {
     die("File koneksi.php tidak ditemukan di path '../lib/koneksi.php'");
 }
